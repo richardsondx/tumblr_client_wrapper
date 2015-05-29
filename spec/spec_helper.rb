04b175a7424844b85b3ed43dd37703fbea911ef3
evenix <richardsondx@gmail.com>
2015-05-29 03:22:11 -0400

    updated rakefile and gemspec

---
+++ spec/spec_helper.rb
@@ -0,0 +1,11 @@
+
+ENV['RAILS_ENV'] ||= 'test'
+
+require 'rspec/rails'
+
+RSpec.configure do |config|
+  config.mock_with :rspec
+  config.use_transactional_fixtures = true
+  config.infer_base_class_for_anonymous_controllers = false
+  config.order = "random"
+end
